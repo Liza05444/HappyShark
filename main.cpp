0cd22b18bfd210bb6def7eebb4b4cea13e5ef7f7
Liza05444 <ABromberg1@rambler.ru>
2024-03-25 22:48:12 +0300

    Creating a Background

--- main.cpp
+++ main.cpp
@@ -1,29 +1,27 @@
 #include <SFML/Graphics.hpp>
 #include "Game/Game.h"
-using namespace sf;
 
 int main() {
-    std::cout << "Game: \n";
+    /*std::cout << "Game: \n";
     Game g(5);
-    std::cout << "g = " << g.get_a() << std::endl;
+    std::cout << "g = " << g.get_a() << std::endl;*/
 
-    RenderWindow window(VideoMode(400, 400), L"Game", Style::Default);
-    window.setVerticalSyncEnabled(true);
-
-    CircleShape shape(100.f, 3);
-    shape.setPosition(100, 100);
-    shape.setFillColor(Color::Magenta);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Happy Shark");
+    sf::Texture f;
+    f.loadFromFile("Images/Ocean_for_Game.jpg");
+    sf::Sprite fon(f);
 
     while (window.isOpen()) {
-        Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == Event::Closed)
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
         }
-        window.clear(Color::Blue);
-        window.draw(shape);
+        window.clear(sf::Color::White);
+        window.draw(fon);
         window.display();
     }
+
     return 0;
 }
